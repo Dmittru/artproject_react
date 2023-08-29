2073daaca3034b7ae7347d3cc5eaf38410affb69
CERF <limbooo689@gmail.com>
2023-08-29 22:02:40 +0300

    init

---
+++ src/components/UI/NavHeader/NavHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../styles/NavHeader.css'
+
+const NavHeader = () => {
+    return (
+        <header className='NavHeader'>
+
+        </header>
+    );
+};
+
+export default NavHeader;
